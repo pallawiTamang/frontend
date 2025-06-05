1264af43f49e044de7fcaddd97c1131202fe4009
Pallawi Tamang Dong <ctit24b.plw@ismt.edu.np>
2025-06-06 01:13:51 +0545

    home ma new change

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react"
 
 
 function Home(){
-    return (
-        <h1>This is home page</h1>
-    )
+    
+ const [number,setNumber] = useState(1000) //[0,f]
+
+function increaseMyValue(){
+        setNumber(number +1)
+}
+function decreaseMyValue(){
+    setNumber(number -1)
+}
+return(
+    <div>
+    <h1>{number}</h1>
+    <button onClick={increaseMyValue}></button>
+    <button onClick={decreaseMyValue}></button>
+    </div>
+)
 }
 
 export default Home
